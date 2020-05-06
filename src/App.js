753ab64509e653f2568ad263ac7260023407efb8
debo1988 <58120831+debo1988@users.noreply.github.com>
2020-05-06 09:11:22 +0300

    add file to website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+import About from './pages/About';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import OurProjects from './pages/OurProjects';
+import Riviera from './pages/Riviera';
+import ContactUs from './pages/ContactUs';
+import SingleProject from './pages/SingleProject';
+import SingleRoom from './pages/SingleRoom'
+
+import {Route, Switch} from 'react-router-dom';
+
+
+import Navbar from './components/Navbar';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer'
+// import {Carousel} from 'react-boostrap';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+   
+    <Switch>
+    <Route exact path="/" component={Home}/>
+      <Route exact path="/about" component={About}/>
+      <Route exact path="/ourprojects/" component={OurProjects}/>
+      <Route exact path="/rooms/:slug" component={SingleRoom}/>
+      <Route exact path="/riviera" component={Riviera}/>
+      <Route exact path="/contactus" component={ContactUs}/>
+      {/* <Route exact path="/singleproject" component={SingleProject}/> */}
+      <Route component={Error}/>
+    </Switch>
+ <Footer/>
+
+    </>
   );
 }
 
---
+++ src/pages/OurProjects.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import FeaturedRooms from '../components/FeaturedRooms'
+
+
+
+
+const Rooms = () =>{
+	return(
+		<>
+	<FeaturedRooms></FeaturedRooms>
+	
+		</>
+	);
+};
+
+export default Rooms;
+
